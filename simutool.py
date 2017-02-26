615b45e09ec38099345df430df21dd3176323e7c
IceDragon <icedragon@quickfox.org>
2017-02-26 04:09:50 +0200

    Minor enhancements

--- simutool.py
+++ simutool.py
@@ -11,13 +11,16 @@ from tools.config import get_master_config
 log = logging.getLogger(__name__)
 
 
-G_HOME = os.environ['HOME'] if 'HOME' in os.environ else '.'
+G_HOME = '/home/pi'
 
 # name of the configuration file
 G_CONFIG_FILENAME = 'simucopter.ini'
 
 # where should the configuration file be located?
 G_CONFIG_PATH = os.path.join(G_HOME, G_CONFIG_FILENAME)
+
+# Simulink build path (where flight mode code is deployed)
+G_SIMULINK_BUILD_PATH = os.path.join(G_HOME, 'simucopter')
 
 # auxiliary code that may be required, but not available from Simulink deployment package
 G_AUX_CODE_DIR = os.path.join(G_HOME, 'simucopter', 'bridge')
@@ -121,19 +124,44 @@ def do_sim_vehicle(postfix=""):
     :rtype: int
     """
     config = get_master_config()
-    cmd = "{} {}".format(config.sim_vehicle, postfix).strip()
     old_cwd = os.getcwd()
     os.chdir(config.arducopter_dir)
 
-    exit_code = os.system(cmd)
+    import subprocess
+    proc = subprocess.Popen((config.sim_vehicle, postfix), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    process_output, _ = proc.communicate("")
+    log.debug(process_output)
+    exit_code = proc.wait()
     log.warning("WARNING: sim_vehicle process exited with code {}".format(exit_code))
 
     os.chdir(old_cwd)
     return exit_code
 
 
+def do_deploy(argv):
+    fm_hint = argv[2]
+    fm_name = os.path.basename(fm_hint).split('.')[0]
+    fm_path = os.path.join(G_SIMULINK_BUILD_PATH, "{}_ert_rtw".format(fm_name))
+    fm_path_status = "OK" if os.path.isdir(fm_path) else "BAD"
+
+    log.info("--- DEPLOY: {} -----------------------------".format(fm_name))
+    log.info(" > Path: {} [{}]".format(fm_path, fm_path_status))
+    log.info("")
+    if not os.path.isdir(fm_path):
+        log.error("Proposed fm_path is not a directory!!")
+        return 1
+
+    log.info("Running link operation against flight mode path...")
+    do_link_flight_mode(fm_path)
+
+    log.info("Trying to run project...")
+    do_sim_vehicle("-j 4")
+    return 0
+
+
 def do_print_syntax(argv):
-    print "Syntax: {} link <flight mode path>".format(argv[0])
+    print "Syntax: {} deploy <flight mode hint>".format(argv[0])
+    print "        {} link <flight mode path>".format(argv[0])
     print "        {} run".format(argv[0])
     print
 
@@ -146,6 +174,7 @@ def main(argv):
     cmd_map = {
         'link': lambda: do_link_flight_mode(argv[2]),
         'run': lambda: do_sim_vehicle("-j 4"),
+        'deploy': lambda: do_deploy(argv),
     }
 
     cmd = argv[1]
@@ -160,7 +189,12 @@ def main(argv):
     log.info("CWD:           {}".format(os.getcwd()))
     log.info("ArduPilot dir: {}".format(get_master_config().ardupilot_dir))
     log.info("Command:       {}".format(cmd))
-    return cmd_map[cmd.lower()]()
+
+    try:
+        return cmd_map[cmd.lower()]()
+    except Exception as ex:
+        log.exception("Exception: ".format(ex))
+        return 3
 
 
 if __name__ == '__main__':
